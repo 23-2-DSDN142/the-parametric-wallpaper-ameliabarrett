6c3d874ecffd2ac018c04bb53b555761f3f7d482
ameliabarrett <ameliarbarrett@icloud.com>
2023-08-02 11:11:43 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,6 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let planetX  =160;
+let rocketY = 60;
 
 
 function setup_wallpaper(pWallpaper) {
@@ -15,67 +15,55 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(53,53,53); //black background
+  background(24,47,102); // navy blue
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+  
+  
+
+ stroke(253, 255, 199); //cream stars
+  drawStar(40,40,10);
+
  
-  fill(255,169,1); //yellow fire underneath
-  stroke(255,127,1); //orange fire outline
-  strokeWeight(5);
-  beginShape();
-  curveVertex(100,185);
-  curveVertex(100,185); //start
-  curveVertex(97,178);
-  curveVertex(90,168);
-  curveVertex(90,160);
-  curveVertex(100,153); //middle
-  curveVertex(110,160);
-  curveVertex(110,168);
-  curveVertex(103,178);
-  curveVertex(100,185); //end
-  curveVertex(100,185);
+
+  fill(253, 255, 199) //cream
+  noStroke();
+  drawPlanet(planetX,75,50,50);
+   
+
+  translate(100, 55);
+  fill(251,255,194);
+  noStroke();
+  beginShape(); //star2
+  vertex(-5, 5); //a = 10, b=35, c=8 d=0
+  vertex(0, 17.5);
+  vertex(5, 5);
+  vertex(17.5, 0);
+  vertex(5, -4);
+  vertex(0, -17.5);
+  vertex(-5, -4);
+  vertex(-17.5, 0);
   endShape();
-
-fill(73,171,129); //darker green rocket body
- noStroke();
- beginShape();
- curveVertex(75,50);
- curveVertex(75,50);
- curveVertex(70,70);
- curveVertex(78,140);
- curveVertex(122,140);
- curveVertex(130,70);
- curveVertex(125,50);
- curveVertex(125,50);
- endShape();
-
- fill(254,46,46); //brigher red rocket top/wings
- noStroke();
- triangle(100,20,75,50,125,50); //top
- triangle(72,115,60,157,75,150); //left wing
- triangle(128,115, 140, 157, 125,150); // right wing
- triangle (85,120,100,161,115,120); // middle wing
-
- fill(254,46,46); //brigher red rocket top/wings
- noStroke();
- beginShape();
- curveVertex(60,157);
- curveVertex(60,157);
- curveVertex(70,120);
- curveVertex(72,115);
- curveVertex(72,115);
- endShape();
-
- fill(255,255,0); //yellow
- ellipse(100,80,40,40); //circle outline
-
- fill(42,202,234); // blue
- ellipse(100,80,30,30); //window
-
-
-
 
 }
 
+function drawStar(x, y, s) { // star
+  
+  line(x, y - s, x, y + s);
+  line(x - s, y, x + s, y);
+  line(x - s, y -s, x + s, y + s);
+ line(x + s, y - s, x -s, y + s);
 
+ }
+ 
+ function drawPlanet(x,y, l, w) { // planets
+  ellipse(x,y,l,w); //175, 75, 50, 50
+
+  fill(235, 240, 163);
+  ellipse(planetX-13,65,15,12);
+  ellipse(planetX+15,65,12,8);
+  ellipse(planetX+12,78,11,7);
+  ellipse(planetX,90,15,10);
+
+ }
